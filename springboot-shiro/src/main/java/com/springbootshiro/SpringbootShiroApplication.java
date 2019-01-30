f29a1675a2e88842d5ab4966c23f4f48b6d32349
ZzzLJ <ZzzLJDec25th@gmail.com>
2019-01-30 12:29:08 +0800

    Shiro的使用及Spring Boot整合Shiro

--- springboot-shiro/src/main/java/com/springbootshiro/SpringbootShiroApplication.java
+++ springboot-shiro/src/main/java/com/springbootshiro/SpringbootShiroApplication.java
@@ -1,11 +1,17 @@
 package com.springbootshiro;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootShiroApplication {
+public class SpringbootShiroApplication implements CommandLineRunner{
     public static void main(String[] args) {
         SpringApplication.run(SpringbootShiroApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
     }
 }
