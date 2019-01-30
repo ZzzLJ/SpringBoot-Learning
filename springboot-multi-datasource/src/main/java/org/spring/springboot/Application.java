7c8c2273b1607d162c7800fd50d1b740f4705876
ZzzLJ <ZzzLJDec25th@gmail.com>
2019-01-30 12:24:53 +0800

    Spring Boot整合Mybatis实现Druid多数据源

--- springboot-multi-datasource/src/main/java/org/spring/springboot/Application.java
+++ springboot-multi-datasource/src/main/java/org/spring/springboot/Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
     public static void main(String[] args) {
         // 程序启动入口
-        // 启动嵌入式的 Tomcat 并初始化 Spring 环境及其各 Spring 组件
+        // 启动嵌入式的Tomcat并初始化Spring环境及其各Spring 组件
         SpringApplication.run(Application.class,args);
     }
 }
