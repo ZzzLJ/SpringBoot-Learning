5e32ac10a6a5253e3bf0562fac331eb54d4ab40f
ZzzLJ <ZzzLJDec25th@gmail.com>
2019-01-30 12:26:24 +0800

    Spring Boot整合Mybatis

--- springboot-mybatis/src/main/java/com/springbootmybatis/SpringbootMybatisApplication.java
+++ springboot-mybatis/src/main/java/com/springbootmybatis/SpringbootMybatisApplication.java
@@ -1,7 +1,6 @@
 package com.springbootmybatis;
 
 import org.mybatis.spring.annotation.MapperScan;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
