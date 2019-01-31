6f3d34c8b5103b0dbc9a79edde37021d40f98899
ZzzLJ <ZzzLJDec25th@gmail.com>
2019-01-31 14:07:20 +0800

    Spring Boot集成Thymeleaf、Freemarker

--- springboot-thymeleaf_freemarker/src/main/java/com/springboot/thymeleaf_freemarker/ThymeleafFreemarkerApplication.java
+++ springboot-thymeleaf_freemarker/src/main/java/com/springboot/thymeleaf_freemarker/ThymeleafFreemarkerApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ThymeleafFreemarkerApplication implements CommandLineRunner {
-
     public static void main(String[] args) {
         SpringApplication.run(ThymeleafFreemarkerApplication.class, args);
     }
